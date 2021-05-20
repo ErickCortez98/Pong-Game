c6b8fc68021d36c06e0e362efae5003c512c86cc
ErickCortez98 <erick-cortez98@hotmail.com>
2021-05-20 16:16:43 -0500

    added rudimentary main menu

--- src/Pong/Tennis.java
+++ src/Pong/Tennis.java
@@ -12,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -51,8 +52,15 @@ public class Tennis extends Application{
 	TennisPlayer playerLeft;
 	TennisPlayer playerRight;
 	
-	Group root;
+	Group rootGamePlay;
+	Group rootMenu;
 	
+	private GraphicsContext gcMenu;
+	private Canvas canvasMenu;
+	private Text nameOfGame;
+	private Button playButton;
+	private Button settingsButton;
+
 	//Random class
 	Random rand;
 	
@@ -94,34 +102,21 @@ public class Tennis extends Application{
 		
 		// Create group that will contain a lot of child nodes and include the 
 		// gamePlayScene into this new group
-		root = new Group();
-		Scene gamePlayScene = new Scene(root);
+		rootGamePlay = new Group();
+		rootMenu = new Group();
+		
+		Scene menuScene = new Scene(rootMenu);
+		Scene gamePlayScene = new Scene(rootGamePlay);
+		
+		//Creating elements that will be part of the menu scene
+		createMenuGroup(primaryStage, gamePlayScene);
+		
+		//Creating elements that will be part of the game play scene
+		createGamePlayGroup(primaryStage);
 		
 		// Add the scene to the stage
-		primaryStage.setScene(gamePlayScene);
-		
-		//Canvas is created to be able to handle animations
-		canvas = new Canvas(500, 500);
-		root.getChildren().add(canvas);
-	
-		// Get graphics context object from the canvas
-		gc = canvas.getGraphicsContext2D();
-		
-		// Setting background color
-		gc.setFill(Color.BLACK);
-		gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-				
-				
-		//Drawing rectangles at the initial positions
-		playerLeft.getPlayerRectangle().setFill(Color.WHITE);
-		playerRight.getPlayerRectangle().setFill(Color.WHITE);
-		
-		// Adding circle to the root group
-		root.getChildren().add(gameBall.getTennisBall());
-		
-		//Adding rectangles to root group 
-		root.getChildren().add(playerLeft.getPlayerRectangle());
-		root.getChildren().add(playerRight.getPlayerRectangle());
+		primaryStage.setScene(menuScene);
+		//primaryStage.setScene(gamePlayScene);
 		
 		// Key Handlers
 		// When a key is pressed we add that input to the inputList
@@ -159,16 +154,20 @@ public class Tennis extends Application{
 			@Override
 			public void handle(long now) {
 				// Method that actually handles animation
+				if(primaryStage.getScene() == gamePlayScene) {
+					// Start Game
+					
+					//Run animation of both players
+					runAnimationPlayer1();
+					runAnimationPlayer2();
+					
+					//Animating the ball
+					gameBall.changePositionBall(movingAngle);
+					
+					//Checking for collisions
+					checkCollisions();
+				}
 				
-				//Run animation of both players
-				runAnimationPlayer1();
-				runAnimationPlayer2();
-				
-				//Animating the ball
-				gameBall.changePositionBall(movingAngle);
-				
-				//Checking for collisions
-				checkCollisions();
 			}
 			
 		}.start();
@@ -251,7 +250,7 @@ public class Tennis extends Application{
 		}
 		playerScored.setFont(Font.font("Verdana", 25));
 		playerScored.setFill(Color.WHITE);
-		root.getChildren().add(playerScored);
+		rootGamePlay.getChildren().add(playerScored);
 		
 		restartGame();
 	}
@@ -282,6 +281,63 @@ public class Tennis extends Application{
 		if(!(playerRight.getPlayerRectangle().getY() > 500 - TennisPlayer.PLAYER_HEIGHT)) {
 			playerRight.getPlayerRectangle().setY(playerRight.getPlayerRectangle().getY()+5);
 		}
+	}
+	
+	private void createMenuGroup(Stage primaryStage, Scene gamePlayScene) {
+		// Canvas is created to be able to handle animations
+		canvasMenu = new Canvas(500, 500);
+		rootMenu.getChildren().add(canvasMenu);
+		
+		// Get graphics context object from the canvas
+		gcMenu = canvasMenu.getGraphicsContext2D();
+						
+		// Setting background color
+		gcMenu.setFill(Color.BLACK);
+		gcMenu.fillRect(0, 0, canvasMenu.getWidth(), canvasMenu.getHeight());
+		
+		//Label with name of the game
+		nameOfGame = new Text(120,115,"Pong Game");
+		nameOfGame.setFont(Font.font("Verdana", 45));
+		nameOfGame.setFill(Color.WHITE);
+		
+		playButton = new Button("Play Game");
+		playButton.setLayoutX(200);
+		playButton.setLayoutY(200);
+		
+		settingsButton = new Button("Settings");
+		settingsButton.setLayoutX(208);
+		settingsButton.setLayoutY(250);
+		
+		playButton.setOnAction(e -> primaryStage.setScene(gamePlayScene));
+				
+		// Add text and buttons to the root
+		rootMenu.getChildren().addAll(nameOfGame, playButton, settingsButton);
+		//playButton.setOnAction(e -> );
+	}
+	
+	private void createGamePlayGroup(Stage primaryStage) {
+		//Canvas is created to be able to handle animations
+		canvas = new Canvas(500, 500);
+		rootGamePlay.getChildren().add(canvas);
+			
+		// Get graphics context object from the canvas
+		gc = canvas.getGraphicsContext2D();
+				
+		// Setting background color
+		gc.setFill(Color.BLACK);
+		gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+						
+						
+		//Drawing rectangles at the initial positions
+		playerLeft.getPlayerRectangle().setFill(Color.WHITE);
+		playerRight.getPlayerRectangle().setFill(Color.WHITE);
+				
+		// Adding circle to the root group
+		rootGamePlay.getChildren().add(gameBall.getTennisBall());
+				
+		//Adding rectangles to root group 
+		rootGamePlay.getChildren().add(playerLeft.getPlayerRectangle());
+		rootGamePlay.getChildren().add(playerRight.getPlayerRectangle());
 	}
 	
 	public static void main(String[] args) {
